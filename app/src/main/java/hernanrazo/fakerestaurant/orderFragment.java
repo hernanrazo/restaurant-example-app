6dd1d89668ee574e4391e3c21b5f33c65f9417b9
Hernan Razo <hernanrazo@gmail.com>
2018-06-26 00:04:28 -0500

    added all spinners to order fragment.

--- app/src/main/java/hernanrazo/fakerestaurant/orderFragment.java
+++ app/src/main/java/hernanrazo/fakerestaurant/orderFragment.java
@@ -17,8 +17,8 @@ public class orderFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_order, container, false);
-        Spinner appetizerSpinner = view.findViewById(R.id.appetizerSpinner);
 
+        Spinner appetizerSpinner = view.findViewById(R.id.appetizerSpinner);
         ArrayAdapter<CharSequence> appetizerAdapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
                                                                                       R.array.spinnerAppetizerStrings,
                                                                                       android.R.layout.simple_spinner_item);
@@ -26,8 +26,37 @@ public class orderFragment extends Fragment {
         appetizerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         appetizerSpinner.setAdapter(appetizerAdapter);
 
+        Spinner entreeSpinner = view.findViewById(R.id.entreeSpinner);
+        ArrayAdapter<CharSequence> entreeAdapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                                                                                   R.array.spinnerEntreeStrings,
+                                                                                   android.R.layout.simple_spinner_item);
 
+        entreeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        entreeSpinner.setAdapter(entreeAdapter);
 
+        Spinner kidsMenuSpinner = view.findViewById(R.id.kidsMenuSpinner);
+        ArrayAdapter<CharSequence> kidsMenuAdapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                                                                                     R.array.spinnerKidsMenuStrings,
+                                                                                     android.R.layout.simple_spinner_item);
+
+        kidsMenuAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        kidsMenuSpinner.setAdapter(kidsMenuAdapter);
+
+        Spinner sideDishSpinner = view.findViewById(R.id.sideDishSpinner);
+        ArrayAdapter<CharSequence> sideDishAdapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                                                                                     R.array.spinnerSideDishStrings,
+                                                                                     android.R.layout.simple_spinner_item);
+
+        sideDishAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sideDishSpinner.setAdapter(kidsMenuAdapter);
+
+        Spinner desertSpinner = view.findViewById(R.id.desertSpinner);
+        ArrayAdapter<CharSequence> desertAdapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                                                                                   R.array.spinnerDesertStrings,
+                                                                                   android.R.layout.simple_spinner_item);
+
+        desertAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        desertSpinner.setAdapter(desertAdapter);
 
         return view;
     }
