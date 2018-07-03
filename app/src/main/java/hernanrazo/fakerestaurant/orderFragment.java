a8584d60d6088e12f50ed257ece474534d5274c1
Hernan Razo <hernanrazo@gmail.com>
2018-07-02 23:34:05 -0500

    changed onCreate()

--- app/src/main/java/hernanrazo/fakerestaurant/orderFragment.java
+++ app/src/main/java/hernanrazo/fakerestaurant/orderFragment.java
@@ -21,6 +21,7 @@ public class orderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        
 
         View view = inflater.inflate(R.layout.fragment_order, container, false);
 
